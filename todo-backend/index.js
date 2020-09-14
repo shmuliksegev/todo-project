f5b5c58a76785c76d725dfca435454e4785d051e
shmuliksegev <zzaaaazz6@gmail.com>
2020-09-14 22:39:23 +0300

    the full project

--- todo-backend/index.js
+++ todo-backend/index.js
@@ -1,60 +1,60 @@
+const express = require("express");
+const cors = require("cors");
 
-const express = require("express")
-const app = express()
-const bodyParser = require("body-parser")
-const PORT = process.env.PORT || 3000
-const db = require("./models/")
+const app = express();
+const bodyParser = require("body-parser");
+const PORT = 4000;
+const db = require("./models/");
 
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 function success(res, payload) {
-  return res.status(200).json(payload)
+  return res.status(200).json(payload);
 }
 
-app.get("/todos", async (req, res, next) => {
+app.get("/api/todos", async (req, res, next) => {
   try {
-    const todos = await db.Todo.find({})
-    return success(res, todos)
+    const todos = await db.Todo.find({});
+    return success(res, todos);
   } catch (err) {
-    next({ status: 400, message: "failed to get todos" })
+    next({ status: 400, message: "failed to get todos" });
   }
-})
+});
 
-app.post("/todos", async (req, res, next) => {
+app.post("/api/todos", async (req, res, next) => {
   try {
-    const todo = await db.Todo.create(req.body)
-    return success(res, todo)
+    const todo = await db.Todo.create(req.body);
+    return success(res, todo);
   } catch (err) {
-    next({ status: 400, message: "failed to create todo" })
+    next({ status: 400, message: "failed to create todo" });
   }
-})
+});
+app.delete("/api/todos/all", async (req, res, next) => {
+  try {
+    await db.Todo.deleteMany({});
+    return success(res, "todo delete all");
+  } catch (err) {
+    next({ status: 400, message: "failed to delete all" });
+  }
+});
 
-app.put("/todos/:id", async (req, res, next) => {
+app.delete("/api/todos/:id", async (req, res, next) => {
   try {
-    const todo = await db.Todo.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    })
-    return success(res, todo)
+    await db.Todo.findByIdAndRemove(req.params.id);
+    return success(res, "todo deleted!");
   } catch (err) {
-    next({ status: 400, message: "failed to update todo" })
+    next({ status: 400, message: "failed to delete todo" });
   }
-})
-app.delete("/todos/:id", async (req, res, next) => {
-  try {
-    await db.Todo.findByIdAndRemove(req.params.id)
-    return success(res, "todo deleted!")
-  } catch (err) {
-    next({ status: 400, message: "failed to delete todo" })
-  }
-})
+});
 
 app.use((err, req, res, next) => {
   return res.status(err.status || 400).json({
     status: err.status || 400,
     message: err.message || "there was an error processing request",
-  })
-})
+  });
+});
 
 app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`)
-})
+  console.log(`listening on port ${PORT}`);
+});
